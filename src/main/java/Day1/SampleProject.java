89040e645dbfaf7e6bbd294d1c356d8062e090a9
priyankaReddy447 <priyankachirra14@gmail.com>
2026-04-10 08:55:09 +0530

    priyanka

--- src/main/java/Day1/SampleProject.java
+++ src/main/java/Day1/SampleProject.java
@@ -1,7 +1,9 @@
 package Day1;
 
 public class SampleProject {
-
+	public void display() {
+		System.out.println("purna");
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("hiii...");
